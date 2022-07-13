769d087cfd52624d285cfc0cb3332866e89ab8e5
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-13 14:47:50 -0600

    Time: 56 ms (45.58%), Space: 13.8 MB (88.15%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -4,12 +4,9 @@ class Solution:
             return strs[0]
         final = ''
         minString = min(strs, key=len)
-        print(minString)
         strs.remove(minString)
-        print(strs)
         for idy, y in enumerate(strs):
             for idx, x in enumerate(minString):
-                print(minString[idx], '==', y[idx])
                 if minString[idx] == y[idx]:
                     final = minString[:idx+1]
                 elif minString[idx] != y[idx] and len(minString) == 1: 
@@ -18,7 +15,6 @@ class Solution:
                 else:
                     break
             minString = final 
-            print(minString)
         return final
             
                 
