7f9aaf7a330326f8e09b1449f26af87035eac597
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-12 19:07:44 -0600

    Time: 70 ms (60.20%), Space: 14 MB (30.61%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,3 +1,6 @@
+#First Approach 
+
+'''
 class Solution:
     def romanToInt(self, s: str) -> int:
         num = 0
@@ -30,5 +33,23 @@ class Solution:
                 num = num + 1000
             print(num, x, s[idx - 1])
         return num
-            
+'''           
         
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
