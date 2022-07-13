b3d955ab1afd4b852d592acf13ea91540dd15da1
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-12 18:04:13 -0600

    Time: 64 ms (91.78%), Space: 13.9 MB (58.82%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,6 +1,24 @@
 # first approach with string
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
         if str(x) == str(x)[::-1]:
             return True
         return False
+
+
+# using integer
+'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+
+        inputNum = x
+        newNum = 0
+        while x>0:
+            newNum = newNum * 10 + x%10
+            x = x//10
+        return newNum == inputNum
+'''
