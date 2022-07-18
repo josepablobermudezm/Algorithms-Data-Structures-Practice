8f85de5da172054f42f782b6afa0eb93d80b026e
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-18 16:55:01 -0600

    Time: 41 ms (74.59%), Space: 13.9 MB (58.40%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for idx in range(len(digits)-1, -1, -1):
+            if digits[idx] < 9:
+                digits[idx] += 1
+                for y in range(idx+1, len(digits)):
+                    digits[y] = 0
+                return digits
+        
+        for i in range(len(digits)):
+            digits[i] = 0
+        digits.insert(0, 1)
+        
+        return digits
+                    
+        
