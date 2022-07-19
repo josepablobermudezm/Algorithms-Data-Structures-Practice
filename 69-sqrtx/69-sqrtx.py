4c394398ee95071e104529ae9e4ac484218e9018
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-19 15:00:13 -0600

    Time: 45 ms (81.56%), Space: 14 MB (11.09%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,3 +1,5 @@
+'''
+#First Approach
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 0:
@@ -9,5 +11,16 @@ class Solution:
             r = c * c
             c += 1
         return c-1 if c * c <= x else c-2
-            
-        
+
+'''
+class Solution:            
+    def mySqrt(self, x):
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r-l)//2
+            if mid * mid <= x < (mid+1)*(mid+1):
+                return mid
+            elif x < mid * mid:
+                r = mid - 1
+            else:
+                l = mid + 1
