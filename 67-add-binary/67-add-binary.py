4909f9c76f21c328b11805f60f6b06a89a90408d
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-19 00:09:12 -0600

    Time: 59 ms (34.63%), Space: 13.9 MB (72.46%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,3 +1,5 @@
+'''
+#First Approach
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
@@ -43,4 +45,23 @@ class Solution:
             
         return ''.join([str(a) for a in array])
 
-        
+'''
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry %2)
+            carry //= 2
+
+        return result[::-1]
