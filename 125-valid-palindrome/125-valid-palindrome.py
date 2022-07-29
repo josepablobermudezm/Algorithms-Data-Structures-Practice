7656932e4d4916072610aa5668c61367313de0ba
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-28 21:34:25 -0600

    Time: 65 ms (66.13%), Space: 14.5 MB (57.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,33 @@
+#First Approach
+
+'''
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         a = ''.join(filter(str.isalnum, s)).lower()
         print(a)
         if a == a[::-1]:
             return True
-        
         return False
+'''
+
         
+# Most Optimal
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        anchor = len(s) - 1
+
+        for i in range(len(s)):
+            if s[i].isalnum():
+                for j in range(anchor, -1, -1):
+                    if i == j:
+                        return True
+                    if s[j].isalnum():
+                        if s[i] == s[j]:
+                            anchor = j - 1
+                            break
+                        else:
+                            return False
+        
+        return True # handle ' '
